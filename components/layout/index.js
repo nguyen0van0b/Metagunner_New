7f336d30db63d283be606e42688dab9b95474b1e
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-02-26 18:39:26 +0700

    tokenomics

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,21 @@
 import { useEffect } from 'react';
 import AOS from "aos";
+import style from './layout.module.scss'
+const BACKGROUNDMB = '/assets/BACKGROUND-MOBILE.webp'
+const BACKGROUND = '/assets/BACKGROUND.webp'
 
 function Layout({children}){
         useEffect(() => {
                 AOS.init();
                 AOS.refresh();
               }, []);
-        return children
+        return (
+                <div className={style.background} >
+                     <img className={style.backgroundPC} src={BACKGROUND} alt=""/>
+                     <img className={style.backgroundMB} src={BACKGROUNDMB} alt=""/>
+                        {children}
+                </div>
+        )
 }
 
 export default Layout
